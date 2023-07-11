9336d8adc391d00d5c45e98d0f4db5fcad680c8c
Ace Rye Skywalker <Reply_M@iCLoud.com>
2023-07-11 11:56:14 +0800

    init

--- embeddings.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding=utf-8
-from text2vec import SentenceModel
-
-def load_embeddings_model(model_path: str, device: str):
-    if device == "gpu":
-        device = "cuda"
-    model = SentenceModel(model_path, max_seq_length=1024, device=device)
-    return model
