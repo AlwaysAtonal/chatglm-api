634470c039affb0165ef5ddddeccc9cbbbd845c8
Ace Rye Skywalker <Reply_M@iCLoud.com>
2023-07-13 19:57:21 +0800

    Update utils.py

--- app/libs/utils.py
+++ app/libs/utils.py
@@ -1,13 +1,14 @@
 import time
 import torch
 from fastapi import BackgroundTasks
+from .config import env
 
 last_gc = 0
 
 def torch_gc():
     # 使用 last_gc 变量来控制 gc 的频率，不多于 1min 一次
     global last_gc
-    if time.time() - last_gc > 60:
+    if time.time() - last_gc > env.gc_time:
         last_gc = time.time()
         if torch.cuda.is_available():
             device = torch.cuda.current_device()
