ca51af13a3d9aa9329703e81034239747c9ca9df
Ace Rye Skywalker <Reply_M@iCLoud.com>
2023-07-13 19:58:54 +0800

    Update config.py

--- app/libs/config.py
+++ app/libs/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     redis_port: int = 6379
     redis_password: str
     redis_db: int = 1
+    gc_time: int = 1200
     model_path: str = Field(default="THUDM/chatglm-6b", description="模型配置路径")
     devices: str = Field(default="gpu", description="设备")
     gpus: int = Field(default=1, ge=1, description="gpu 数量")
